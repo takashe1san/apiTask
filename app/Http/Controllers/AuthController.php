2f641c4906ecf1aa486a923a5f6f6fa34e9d5415
khaled <takashe1san@gmail.com>
2022-12-19 16:24:50 +0200

    =Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
+    }
+
+    public function signup(Request $request){
+        $roles = [
+            'name'     => 'required',
+            'email'    => ['email', 'required', 'unique:users'],
+            'password' => 'required',
+            'address'  => 'nullable',
+            'city'     => 'required',
+            'type'     => 'in:admin,user'
+        ];
+
+        $request->validate($roles);
+
+        if(User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'address'  => $request->address,
+            'city'     => $request->city,
+            'type'     => $request->type,
+        ])){
+            return response()->json(['msg' => 'Account created!!']);
+        }else{
+            return response()->json(['msg' => 'Something went wrong!!!']);
+        }
+    }
+
+    public function login(Request $request){
+        $credentials = $request->only(['email', 'password']);
+        if(!$token = auth()->attempt($credentials)){
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+
+    public function logout(){
+        auth()->logout();
+    }
+
+    protected function respondWithToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'beared',
+            'expires_in'   => env('JWT_TTL', 60)*60
+        ]);
+    }
+}
